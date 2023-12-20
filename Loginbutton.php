3919e6214f543a63c9ea59ab4b083b05728a3d56
yuna0911 <150318613+yuna0911@users.noreply.github.com>
2023-12-20 09:43:39 +0800

    login button and main layout

---
+++ Loginbutton.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Button</title>
+    <?php include "mainlayout.php";?>
+</head>
+<body>
+<div class="loginTop">
+    <li class="dropdown">
+    <a href="#" class="dropbtn">Login</a>
+    <div class="dropdown-content">
+      <a href="#">Register</a>
+    </div>
+</div>
+    
+</body>
+</html>
