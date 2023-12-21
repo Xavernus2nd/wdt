552b2505b8b85adce955b6da1c86c4422f67da56
Xavernus2nd <46132763+Xavernus2nd@users.noreply.github.com>
2023-12-21 11:04:48 +0800

    file name changes

---
+++ approveQuestionSet.php
@@ -0,0 +1,112 @@
+<?php
+include "connection.php";
+session_start(); //remember to include session code here
+
+// Retrieve unapproved question set data from the database
+$SetToBeApproved = mysqli_fetch_all(mysqli_query($DBconn, "SELECT * FROM Question_Set WHERE SetApprovalStatus = 'PENDING'"), MYSQLI_ASSOC);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Approve Question Set</title>
+    <link rel="stylesheet" href="layout.css">
+    <style>
+        table, th, td {
+        border: 1px solid black;
+        border-collapse: collapse;
+        }
+    </style>
+</head>
+<body>
+    <h2>Approve Question Set</h2>
+    <?php
+    // Dropdown to select question set to be approved
+    echo "<label>Question Set:</label>
+    <form action='' method='GET'>
+    <select name='QuestionSetID' onchange='this.form.submit()'>
+    <option value=''>Select Question Set</option>";
+    foreach($SetToBeApproved as $row){
+        echo "<option value='$row[SetID]'>$row[SetName]</option>";
+    }
+    echo "</select></form><br>";
+    // Display question set details
+    if(isset($_GET["QuestionSetID"])){
+        $QuestionSetID = $_GET["QuestionSetID"];
+        $QuestionsFromSet = mysqli_fetch_all(mysqli_query($DBconn, "SELECT * FROM Question WHERE SetID = '$QuestionSetID'"), MYSQLI_ASSOC);
+        $QuestionSet = mysqli_fetch_assoc(mysqli_query($DBconn, "SELECT * FROM Question_Set WHERE SetID = '$QuestionSetID'"));
+        $QuestionNumber = mysqli_fetch_assoc(mysqli_query($DBconn, "SELECT QuestionNumber FROM Question WHERE SetID = '$QuestionSetID'"));
+        $currentQuestionIndex = isset($_GET['QuestionNumber']) ? $_GET['QuestionNumber'] : 0; //checks if current question number is set, if not set to 0   
+        if(!empty($QuestionsFromSet)){
+            echo "<div class='aqbox'><center><h2>Question Set: $QuestionSet[SetName]</h2></center>";
+            //checks for if action is set and does respective index update
+            if(isset($_GET['action'])){
+                if($_GET['action'] == 'Previous Question'){
+                    if($currentQuestionIndex > 0){
+                        $currentQuestionIndex--;
+                    }
+                }
+                else if($_GET['action'] == 'Next Question'){
+                    if($currentQuestionIndex < count($QuestionsFromSet)-1){
+                        $currentQuestionIndex++;
+                    }
+                }
+            }
+            $currentQuestion = $QuestionsFromSet[$currentQuestionIndex];
+            echo "<div class='aqset'><h3>Question $currentQuestion[QuestionNumber]</h3>
+            <p>$currentQuestion[Question]</p>
+            <h4>Answer:</h4><div class='aqset'>";
+            for ($i = 0; $i <= 3; $i++) {
+                $optionArray = array("OptionA", "OptionB", "OptionC", "OptionD");
+                $answer = "$optionArray[$i]";
+                $isChecked = $currentQuestion[$answer] == $currentQuestion['Answer'] ? "checked" : ""; //checks if answer is correct
+                echo "<label>
+                <input type='radio' name='answer' value='$currentQuestion[$answer]' $isChecked disabled>
+                $currentQuestion[$answer]
+              </label><br>";
+            }//echos the question number and question
+            echo "</div></div>";
+            //specific question selection box
+            echo "<div class='aqnav'><form action='' method='GET'>
+            <input type='hidden' name='QuestionSetID' value='$QuestionSetID'>
+            <label>Go to Question:</label>
+            <select name='QuestionNumber' onchange='this.form.submit()'>
+            <option value=''>Select Question</option>
+            ";
+            foreach($QuestionsFromSet as $row){
+                echo "<option value='$row[QuestionNumber]'>$row[QuestionNumber]</option>";
+            }
+            echo "</select></form>";
+            //displays the previous and next question buttons
+            echo "<form action='' method='GET'> 
+            <input type='hidden' name='QuestionSetID' value='$QuestionSetID'>
+            <input type='hidden' name='QuestionNumber' value='$currentQuestionIndex'>
+            <input type='submit' name='action' class='button' value='Previous Question'>
+            <input type='submit' name='action' class='button' value='Next Question'>
+            </form></div><br>";
+            echo "<center><table class='approveinfotable'>
+            <td>Topic Name: $QuestionSet[TopicID]</td>
+            <td>Submitted By: $QuestionSet[TeacherUsername]</td>
+            <table><br>";
+            echo "<form action='' method='POST'>
+            <input type='hidden' name='QuestionSetID' value='$QuestionSetID'>
+            <input type='submit' name='Approve' class='approve' value='Approve Question Set'>
+            <input type='submit' name='Reject' class='reject' value='Reject Question Set'>
+            </form></center></div>";
+            if (isset($_POST['Approve'])){
+                mysqli_query($DBconn, "UPDATE Question_Set SET SetApprovalStatus = 'APPROVED' WHERE SetID = '$QuestionSetID'");
+                echo "<script>alert('Question Set Approved!'); window.location.href='ApproveQuestionSet.php'</script>";
+                exit();
+            }
+            else if (isset($_POST['Reject'])){
+                mysqli_query($DBconn, "UPDATE Question_Set SET SetApprovalStatus = 'REJECTED' WHERE SetID = '$QuestionSetID'");
+                echo "<script>alert('Question Set Rejected!'); window.location.href='ApproveQuestionSet.php'</script>";
+                exit();
+            }
+    }
+    }
+    ?>
+    
+</body>
+</html>
---
+++ manageQuestionSetAdmin.php
@@ -0,0 +1,96 @@
+<?php
+session_start();
+include 'connection.php'; //connection script
+include 'deleteQuestionSet.php'; //includes the deletion script
+if(isset($_POST['selecttopic'])){ //checks for if topic is selected and sets the session
+    $SelectedTopicCheck['SelectedTopic']=$_POST['SelectedTopic'];
+    if($SelectedTopicCheck['SelectedTopic']=="Not Selected"){
+        echo "<script>alert('Please select a topic');</script>";
+    }
+    else{
+        $_SESSION['SelectedTopic']=$_POST['SelectedTopic'];
+    }
+}
+if(isset($_SESSION['SelectedTopic'])){ //queries the database for the question sets for the selected topic
+    $SelectedTopicId=$_SESSION['SelectedTopic'];
+    $result = mysqli_query($DBconn, "SELECT Question_Set.SetID, Question_Set.SetName, COUNT(Question.QuestionID) AS NoOfQuestions, Question_Set.TeacherUsername, Question_Set.TopicID, Topic.TopicTitle FROM Question_Set INNER JOIN Topic ON Question_Set.TopicID = Topic.TopicID LEFT JOIN Question ON Question_Set.SetID = Question.SetID WHERE Topic.TopicTitle = '$_SESSION[SelectedTopic]' GROUP BY Question_Set.SetID");
+    $questionSets = mysqli_fetch_all($result, MYSQLI_ASSOC);
+}
+$TopicQuery = mysqli_query($DBconn, "SELECT TopicTitle FROM Topic");
+$Topiclists = mysqli_fetch_all($TopicQuery, MYSQLI_ASSOC);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Manage Question Set</title>
+    <style>
+        table, th, td {
+        border: 1px solid black;
+        border-collapse: collapse;
+        }
+    </style>
+    <link href='layout.css' rel='stylesheet'>
+    <script>
+        function deleteQuestionSet(){
+            var confirmDelete = confirm("Are you sure you want to delete this question set?");
+            if(confirmDelete){
+                return true;
+            }
+            else{
+                alert("No actions were performed.");
+                return false;
+            }
+        }
+        </script>
+</head>
+<body>
+<?php //include 'Loginbutton.php' ?>
+    <div id="container">
+    <h2>View/Manage Question Set</h2>
+    <form action="" method="post">
+        <select name="SelectedTopic">
+            <option value="Not Selected">Please select a topic</option>
+        <?php 
+        foreach ($Topiclists as $topic) {
+            echo "<option value='$topic[TopicTitle]'>$topic[TopicTitle]</option>";
+        }
+        ?>
+        </select>
+        <button type="submit" name="selecttopic" class="button">Select Topic</button>
+    </form> 
+    <?php
+    // Check if a specific topic is selected
+    if (isset($_POST['SelectedTopic'])) {
+        // Display question sets for the selected topic 
+        echo "<h2 style='text-align:center'>Question Sets for $_SESSION[SelectedTopic]</h2>";
+        echo "<table class='table'>   
+                <tr>
+                    <th>Question Set ID</th>
+                    <th>Question Set Name</th>
+                    <th>Number of Questions</th>
+                    <th>Submitted By</th>
+                    <th>Actions</th>
+                </tr>"; //echos the table headers
+        foreach($questionSets as $questionSet){
+            echo"<tr>
+                    <td>$questionSet[SetID]</td>
+                    <td>$questionSet[SetName]</td>
+                    <td>$questionSet[NoOfQuestions]</td>
+                    <td>$questionSet[TeacherUsername]</td>
+                    <td>
+                        <form action='' method='post' onsubmit='return deleteQuestionSet();'>
+                            <input type='hidden' name='DeleteSetID' value='$questionSet[SetID]'>   
+                            <button type='submit' name='delete' class='button'>Delete</button>
+                        </form>
+                    </td>
+                </tr>"; //echos the table data
+    }
+        echo "</table>";
+}
+    ?>
+    </div>
+</body>
+</html>
