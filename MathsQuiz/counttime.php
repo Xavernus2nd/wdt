8e6fd4daa00f6fe754ec0a8b2d876721d2b2ee68
mastura <64648501+yoongzbear@users.noreply.github.com>
2023-12-30 22:02:44 +0800

    Delete MathsQuiz directory

--- MathsQuiz/counttime.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'connection.php';
-date_default_timezone_set("Asia/Kuala_Lumpur");
-session_start();
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['countdown_timer'])) {
-    $_SESSION['countdown_timer'] = max(0, (int)$_POST['countdown_timer']);
-}
-?>
