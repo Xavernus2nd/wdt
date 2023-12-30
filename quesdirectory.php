b2b322d4081c25f97f582cdb2e66591c92be7ab2
mastura <64648501+yoongzbear@users.noreply.github.com>
2023-12-30 22:05:46 +0800

    assessment
    choose question set and mode, answer quiz with directory, answer history

---
+++ quesdirectory.php
@@ -0,0 +1,49 @@
+<?php
+$set = $_POST['setID'];
+$trialID = $_POST['trialID'];
+$mode = $_POST['mode'];
+$currentQuestionNum = $_POST['quesNo'];
+
+//sql to get number questions - Q1, Q2, Q3
+$SQLnumques = "SELECT * FROM question WHERE SetID = '$set';";
+$runSQLnumques = mysqli_query($DBconn, $SQLnumques);
+
+?>
+<table border="1">
+    <?php
+    while ($data = mysqli_fetch_array($runSQLnumques)) {
+        $quesID = $data['QuestionID'];
+        $quesnum = $data['QuestionNumber'];
+
+        //sql to check whether the question is answered 
+        $SQLcheck = "SELECT * FROM student_answer WHERE TrialID = '$trialID' AND QuestionID = '$quesID';";
+        $runSQLcheck = mysqli_query($DBconn, $SQLcheck);
+        $num = mysqli_num_rows($runSQLcheck);
+
+        ?>
+        <tr><td><form id='directoryForm' method='post' action=''>
+        <input type="hidden" name="setID" value="<?php echo $set;?>">
+        <input type="hidden" name="trialID" value="<?php echo $trialID;?>">
+        <input type="hidden" name="mode" value="<?php echo $mode;?>">
+        <input type="hidden" name="quesNo" value="<?php echo $quesnum;?>">
+        <input type="hidden" name="beginquiz" value="">
+        <button onclick="directForm()" <?php if ($quesnum == $currentQuestionNum) echo "style='background-color: rgba(71, 168, 237, 0.25); font-weight: bold; color: black;'"; ?>>
+        <?php
+        if ($num > 0) {
+            //if the question is answered
+            echo "Question ".$quesnum."&check;";
+        } else {
+            //if the question is not answered
+            echo "Question ".$quesnum;
+        }
+        echo "</button></form></td></tr>";
+    }
+    ?>
+</table>
+
+<script>
+    //submit form automatically
+    function submitForm() {
+        document.getElementById('directoryForm').submit();
+    }
+</script>
---
+++ score.php
@@ -0,0 +1,41 @@
+<?php
+include 'connection.php';
+$setID = $_POST['setID'];
+$trialID = $_POST['trialID'];
+$timeTaken = $_POST['timeTaken'];
+$mode = $_POST['mode'];
+$username = $_SESSION['StudentUsername'];
+$count = 0;
+$i = 0;
+
+//counting number of questions within the set
+$SQLnum = "SELECT COUNT(QuestionID) FROM question WHERE SetID = '$setID';";
+$runSQLnum = mysqli_query($DBconn, $SQLnum);
+$dataSQLnum = mysqli_fetch_array($runSQLnum);
+$totalques = $dataSQLnum["COUNT(QuestionID)"];
+
+//get IsCorrect, if 1 then count++
+$SQLcorrect = "SELECT * FROM student_answer WHERE TrialID = $trialID";
+$runSQLcorrect = mysqli_query($DBconn, $SQLcorrect);
+
+while($result = mysqli_fetch_array($runSQLcorrect)) {
+    if ($result['IsCorrect'] == 1) {
+        $count++;
+    }
+}
+
+$score = round(($count/$totalques) *100);
+
+//storing score and time taken into trial table 
+$SQLupdate = "UPDATE trial SET Score = $score, TimeTaken = '$timeTaken' WHERE TrialID = $trialID;";
+$runSQLupdate = mysqli_query($DBconn, $SQLupdate);
+?>
+<form id="resultForm" action="resultanswer.php" method="POST">
+    <input type="hidden" name="trialID" value="<?php echo $trialID;?>">
+    <input type="hidden" name="setID" value="<?php echo $setID;?>">
+    <input type="hidden" name="mode" value="<?php echo $mode;?>">
+</form>
+<script>
+    //submit form automatically
+    document.getElementById("resultForm").submit();
+</script>
