6799627c496e21fa52eff940e0ed069b3a0a48f3
yuna0911 <150318613+yuna0911@users.noreply.github.com>
2024-01-02 16:47:59 +0800

    mainlayout things

---
+++ footer.php
@@ -0,0 +1,4 @@
+ <!-- footer for every pages -->
+<p>Copyright 2023 © Group 12</p>
+<p>Disclaimer: We are not responsible for any damages.</p>
+<p>Contact Admin bingojeans@gmail.com for any inquiries.</p>
