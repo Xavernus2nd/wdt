86a83e82a62be78015002b9538847d35c93c4d72
yuna0911 <150318613+yuna0911@users.noreply.github.com>
2024-01-07 05:18:09 +0800

    home file

---
+++ homeA.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mathematics Quiz</title>
+    <link rel="stylesheet" href="layout.css">
+
+
+</head>
+<body>
+<div id="logo"></div>
+
+<nav>
+    <?php include 'nAdmin.php';?>
+</nav>
+
+<h1>Form 4 SPM Mathematics Quiz</h1>
+
+
+<section class="body-section">
+
+<p>Welcome To Form 4 SPM Mathematics Quiz!</p>
+
+</section>
+
+<!-- Logout only for admin -->
+<div class="loginTop">
+    <div class="dropdown-register">
+        <a href="logout.php" id="logout">Logout</a>
+    </div>
+</div>
+
+<footer>
+    <?php include 'footer.php'; ?>
+</footer>
+</body>
+</html>
---
+++ homeT.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mathematics Quiz</title>
+    <link rel="stylesheet" href="layout.css">
+
+
+</head>
+<body>
+<div id="logo"></div>
+
+<nav>
+    <?php include 'nTeacher.php';?>
+</nav>
+
+<h1>Form 4 SPM Mathematics Quiz</h1>
+
+
+<section class="body-section">
+
+<p>Welcome To Form 4 SPM Mathematics Quiz!</p>
+
+</section>
+
+<?php include 'profileBT.php';?>
+
+
+<footer>
+    <?php include 'footer.php'; ?>
+</footer>
+</body>
+</html>
