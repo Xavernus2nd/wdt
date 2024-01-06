ddd77974e2dc0a76b9d232e98be7552b1a7ec37f
yuna0911 <150318613+yuna0911@users.noreply.github.com>
2024-01-07 05:24:48 +0800

    profile button for t and s

---
+++ profileBS.php
@@ -0,0 +1,8 @@
+<div class="loginTop">
+    <li class="dropdown" id="userDropdown">
+        <a href="manage_profile_student.php" class="dropbtn" id="profile">Profile</a>  <!-- modify profile file -->
+        <div class="dropdown-register">
+            <a href="logout.php" id="logout">Logout</a>
+        </div>
+    </li>
+</div>
