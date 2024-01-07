52fa101cbb691856c21fad23e7dc5a4fcc43e717
yuna0911 <150318613+yuna0911@users.noreply.github.com>
2024-01-08 02:49:39 +0800

    home in index file

--- index.php
+++ index.php
@@ -17,8 +17,22 @@
 
     <h1>Form 4 SPM Mathematics Quiz</h1>
 
-    <div class="body-section">
-        <p>Welcome To Form 4 SPM Mathematics Quiz!</p>
+<div class="body-section">
+    <h2 style="font-size: 30px;text-shadow: 2px 2px 4px #000000;">Welcome To Form 4 SPM Mathematics Quiz!</h2>
+    <p style="font-size: 18px;font-family: 'Sofia', sans-serif;">This interactive quiz has been crafted to test your knowledge and understanding of the Form 4 level Mathematics curriculum.<br> Whether you're a student preparing for the SPM examination or someone eager to challenge their mathematical prowess, this quiz is designed to engage and assess your skills.</p>
+
+
+    <p style="font-size: 18px; font-family: 'Sofia', sans-serif;">Feel free to embark on this educational journey, where you'll encounter a series of questions that will both challenge and reinforce your understanding of Form 4 Mathematics.<br> Whether you're aiming for excellence or seeking to reinforce your foundational knowledge, this quiz is an excellent resource for honing your mathematical skills.</p>
+
+    <br>
+    <img src="icons8-book.gif" style="display: block; margin: auto;">
+
+    <br>
+    <br>
+
+    <p style="font-size: 15px; font-family: 'Lucida Console', 'Courier New', monospace; text-align:center; ">Experience the joy of mathematical challenges by <b style="color: blue;"><a href="login.php">logging in</a></b> or <b style="color: blue;"><a href="register.php">register an account</a></b> now for our professionally crafted Mathematics Quiz!</p>
+
+
         <!-- include a home file for the content of home?-->
 </div>
 
