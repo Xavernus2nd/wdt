81ad06bba6b7d5bb2f8cef965fd8dd7ca355b12c
yuna0911 <150318613+yuna0911@users.noreply.github.com>
2024-01-09 18:39:53 +0800

    nav teacher

--- nTeacher.php
+++ nTeacher.php
@@ -1,6 +1,7 @@
 <ul id='navlist'>
     <li><a href="homeT.php">Home</a></li>
     <li><a href="">Upload Question Set</a></li>
+    <li><a href=".php">Manage Question Set</a></li>
     <li><a href="asnwerHistory.php">View Result</a></li>
     <li><a href="">Manage Class</a></li>
 </ul>
