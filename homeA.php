20358388697367876d0bf8b1a717fc8a1e6a9877
yuna0911 <150318613+yuna0911@users.noreply.github.com>
2024-01-09 23:58:49 +0800

    home

--- homeA.php
+++ homeA.php
@@ -1,7 +1,3 @@
-<?php include 'sessionAdmin.php'?>
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- homeT.php
+++ homeT.php
@@ -1,6 +1,3 @@
-<?php include 'sessionTeacher.php'?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
