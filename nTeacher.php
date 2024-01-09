e1cfd04851a45989a63aa5493eeb1449f058f7f8
nxthhh <150322618+nxthhh@users.noreply.github.com>
2024-01-09 19:25:15 +0800

    Update nTeacher.php

--- nTeacher.php
+++ nTeacher.php
@@ -2,6 +2,6 @@
     <li><a href="homeT.php">Home</a></li>
     <li><a href="addQuestionSet">Upload Question Set</a></li>
     <li><a href="manageQuestionSetTeacher.php">Manage Question Set</a></li>
-    <li><a href="asnwerHistory.php">View Student Results</a></li>
-    <li><a href="">Manage Class</a></li>
+    <li><a href="viewResultsTeacher.php">View Student Results</a></li>
+    <li><a href="manageClassTeacher.php">Manage Class</a></li>
 </ul>
