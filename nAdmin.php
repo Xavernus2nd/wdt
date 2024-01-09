28b2b7836a8ae82480364c0f3aa9eb0f80d4becb
Xavernus2nd <46132763+Xavernus2nd@users.noreply.github.com>
2024-01-09 18:29:56 +0800

    Update nAdmin.php

--- nAdmin.php
+++ nAdmin.php
@@ -1,7 +1,7 @@
 <ul id='navlist'>
     <li><a href="homeA.php">Home</a></li>
-    <li><a href="#answerHistory.php">Manage Topic</a></li>
-    <li><a href="manageQuestionSetAdmin.php">Approve Question Set</a></li>
+    <li><a href="manageTopic.php">Manage Topic</a></li>
+    <li><a href="approveQuestionSet.php">Approve Question Set</a></li>
     <li><a href="manageQuestionSetAdmin.php">Manage Question Set</a></li>
-    <li><a href="manageclass.php">Manage Class</a></li> <!-- modify manage class link-->
+    <li><a href="manageClassAdmin.php">Manage Class</a></li> <!-- modify manage class link-->
 </ul>
