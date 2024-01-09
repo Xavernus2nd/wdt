05adae9e1db64e768e27ad955639c91e0cc63a2e
yuna0911 <150318613+yuna0911@users.noreply.github.com>
2024-01-09 18:23:31 +0800

    nAdmin

--- nAdmin.php
+++ nAdmin.php
@@ -1,6 +1,7 @@
 <ul id='navlist'>
     <li><a href="homeA.php">Home</a></li>
-    <li><a href="#answerHistory.php">View Result</a></li>
+    <li><a href="#answerHistory.php">Manage Topic</a></li>
+    <li><a href="manageQuestionSetAdmin.php">Approve Question Set</a></li>
     <li><a href="manageQuestionSetAdmin.php">Manage Question Set</a></li>
     <li><a href="manageclass.php">Manage Class</a></li> <!-- modify manage class link-->
 </ul>
