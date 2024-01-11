5c3d58cddd9a00755c5f77ab7b307d67b4be830c
nxthhh <150322618+nxthhh@users.noreply.github.com>
2024-01-11 23:16:08 +0800

    Update studentListTeacher.php

--- studentListTeacher.php
+++ studentListTeacher.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Student List</title>
     <link href="viewResults.css" rel="stylesheet">
     <link href="layout.css" rel="stylesheet">
 </head>
