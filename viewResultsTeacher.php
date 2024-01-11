b175d625ba57ffb1605969c47f9c49d345b2a778
nxthhh <150322618+nxthhh@users.noreply.github.com>
2024-01-11 12:08:17 +0800

    Update viewResultsTeacher.php

--- viewResultsTeacher.php
+++ viewResultsTeacher.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>View Results</title>
-    <link rel="stylesheet" href="viewResults.css">
     <link rel="stylesheet" href="layout.css">
 </head>
 <body>
