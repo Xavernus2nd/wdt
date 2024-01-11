8a0ac4d455c8b1224901b83a977debc5456531f5
Xavernus2nd <46132763+Xavernus2nd@users.noreply.github.com>
2024-01-11 09:55:51 +0800

    Update nTeacher.php

--- nTeacher.php
+++ nTeacher.php
@@ -1,6 +1,6 @@
 <ul id='navlist'>
     <li><a href="homeT.php">Home</a></li>
-    <li><a href="addQuestionSet">Upload Question Set</a></li>
+    <li><a href="addQuestionSet.php">Upload Question Set</a></li>
     <li><a href="manageQuestionSetTeacher.php">Manage Question Set</a></li>
     <li><a href="viewResultsTeacher.php">View Student Results</a></li>
     <li><a href="manageClassTeacher.php">Manage Class</a></li>
