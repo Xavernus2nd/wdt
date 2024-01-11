6914e64cc731dd313bbc6bb3e01cc3188da82c85
Xavernus2nd <46132763+Xavernus2nd@users.noreply.github.com>
2024-01-11 08:34:38 +0800

    Update profileBS.php

--- profileBS.php
+++ profileBS.php
@@ -1,6 +1,6 @@
 <div class="loginTop">
     <li class="dropdown" id="userDropdown">
-        <a href="manage_profile_student.php" class="dropbtn" id="profile">Profile</a>  <!-- modify profile file -->
+        <a href="manageProfileStudent.php" class="dropbtn" id="profile">Profile</a>  <!-- modify profile file -->
         <div class="dropdown-register">
             <a href="logout.php" id="logout">Logout</a>
         </div>
