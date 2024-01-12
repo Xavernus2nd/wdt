70d77d9b6a3662a84b65949d3c8146cb5b1ff7d0
Xavernus2nd <46132763+Xavernus2nd@users.noreply.github.com>
2024-01-12 21:07:30 +0800

    query fix

--- deleteTopic.php
+++ deleteTopic.php
@@ -2,7 +2,9 @@
 include 'connection.php';
 if(isset($_POST['deleteTopic'])){
     $deletetopicID=$_POST['DeleteTopicID'];
+    $deleteStudentAnswerQuery = mysqli_query($DBconn, "DELETE FROM Student_Answer WHERE QuestionID IN (SELECT QuestionID FROM Question WHERE SetID IN (SELECT SetID FROM Question_Set WHERE topicID='$deletetopicID'))");
     $deleteTopicQuestionQuery = mysqli_query($DBconn, "DELETE FROM Question WHERE SetID IN (SELECT SetID FROM Question_Set WHERE topicID='$deletetopicID')");
+    $deleteTrial = mysqli_query($DBconn, "DELETE FROM Trial WHERE SetID IN (SELECT SetID FROM Question_Set WHERE topicID='$deletetopicID')");
     $deleteTopicQuestionSetQuery = mysqli_query($DBconn, "DELETE FROM Question_Set WHERE TopicID='$deletetopicID'");
     $deleteTopicQuery = mysqli_query($DBconn, "DELETE FROM topic WHERE TopicID='$deletetopicID'");
     if ($deleteTopicQuery && $deleteTopicQuestionSetQuery && $deleteTopicQuestionQuery) {
