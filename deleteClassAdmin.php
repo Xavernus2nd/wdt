56edffa3084b239790858ceec89f0e167fef083a
nxthhh <150322618+nxthhh@users.noreply.github.com>
2024-01-13 11:05:55 +0800

    simplified code

--- deleteClassAdmin.php
+++ deleteClassAdmin.php
@@ -7,21 +7,10 @@ $sql = "DELETE FROM class WHERE ClassID='$id'";
 $removestudent = "UPDATE student SET ClassID = NULL WHERE ClassID = '$id'";
 $removestudentresult = mysqli_query($DBconn, $removestudent);
 
-if ($removestudentresult) {
-   if (mysqli_query($DBconn,$sql)) {
-      echo "<script>alert('Class Deleted!');window.location.href='manageClassAdmin.php'</script>";
-   } else {
-      echo "Error deleting class! " . mysqli_error($DBconn);
-   }
+if ($removestudentresult && mysqli_query($DBconn,$sql)) {
    echo "<script>alert('Class Deleted!');window.location.href='manageClassAdmin.php'</script>";
-} else {
-   echo "Error deleting class!: " . mysqli_error($DBconn);
-}
-
-if (mysqli_query($DBconn,$sql)) {
-   echo "<script>alert('Class Deleted!');window.location.href='manageClassAdmin.php'</script>";
-} else {
+ } else {
    echo "Error deleting class! " . mysqli_error($DBconn);
-}
+ }
 
 ?>
