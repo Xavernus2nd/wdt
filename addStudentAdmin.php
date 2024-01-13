3f59b2b332239cdd58dda506283db3a4585351f3
nxthhh <150322618+nxthhh@users.noreply.github.com>
2024-01-13 23:25:08 +0800

    Update addStudentAdmin.php

--- addStudentAdmin.php
+++ addStudentAdmin.php
@@ -38,16 +38,16 @@ if(isset($_POST['add_student'])){
         if ($row2['count'] == 1){
             $result = mysqli_query($DBconn, $query);
             echo "<script>alert('Student added successfully')</script>";
-            echo "<script>window.open('addStudentAdmin.php', '_self')</script>";
+            echo "<script>window.open('manageClassAdmin.php', '_self')</script>";
         }
         else{
             echo "<script>alert('Class does not exist')</script>";
-            echo "<script>window.open('addStudentAdmin.php', '_self')</script>";
+            echo "<script>window.open('manageClassAdmin.php', '_self')</script>";
         }
     }
     else{
         echo "<script>alert('Student does not exist')</script>";
-        echo "<script>window.open('addStudentAdmin.php', '_self')</script>";
+        echo "<script>window.open('manageClassAdmin.php', '_self')</script>";
     }
     //if($result == 1){
     //    echo "<script>alert('Student added successfully')</script>";
