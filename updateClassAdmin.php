3d1856f68f671e2e3a92f49266c5d046546cea3c
nxthhh <150322618+nxthhh@users.noreply.github.com>
2024-01-13 18:39:43 +0800

    Update updateClassAdmin.php

--- updateClassAdmin.php
+++ updateClassAdmin.php
@@ -2,33 +2,24 @@
 include("connection.php");
 $updateclass = $_POST['ClassName'];
 $classID = $_POST['ClassID'];
-$updateteachername = $_POST['TeacherFullName'];
 $updateusername = $_POST['TeacherUsername'];
 
 
-$sql = "UPDATE class SET ClassName = '$updateclass' WHERE ClassID = '$classID'";
-$sql2 = "UPDATE Class a SET a.TeacherUserName = (SELECT b.TeacherUserName FROM Teacher b WHERE b.TeacherFullName ='$updateteachername') WHERE a.ClassID ='$classID'";
-$exist = "SELECT COUNT(1) AS count FROM teacher WHERE TeacherFullName = '$updateteachername'";
+$sql = "UPDATE class SET ClassName = '$updateclass', TeacherUsername = '$updateusername' WHERE ClassID = '$classID'";
+$exist = "SELECT COUNT(1) AS count FROM teacher WHERE TeacherUsername = '$updateusername'";
 
 $result = mysqli_query($DBconn, $sql);
-$result2 = mysqli_query($DBconn, $sql2);
-$result3 = mysqli_query($DBconn, $exist);
+$result2 = mysqli_query($DBconn, $exist);
 
-$row = mysqli_fetch_assoc($result3);
+$row = mysqli_fetch_assoc($result2);
 
 if ($row['count'] == 1){
-   if ($result2) {
-      echo "<script>alert('Update Successful!');window.location.href='manageClassAdmin.php'</script>";
+      if ($result) {
+         echo "<script>alert('Update Successful!');window.location.href='manageClassAdmin.php'</script>";
+      } else {
+         echo "Update Failed!: " . mysqli_error($DBconn);
+      }
    } else {
-      echo "Error Updating!: " . mysqli_error($DBconn);
+      echo "<script>alert('Teacher does not exist!');window.location.href='manageClassAdmin.php'</script>";
    }
-if ($result) {
-   echo "<script>alert('Update Successful!');window.location.href='manageClassAdmin.php'</script>";
-} else {
-   echo "Error Updating!: " . mysqli_error($DBconn);
-}
-}
-else{
-   echo "<script>alert('Teacher does not exist!');window.location.href='manageClassAdmin.php'</script>";
-}
 ?>
