6edbd53b92aad57b5a8f5a6283ba3a1bb4a90788
nxthhh <150322618+nxthhh@users.noreply.github.com>
2024-01-14 19:06:22 +0800

    Update updateClassAdmin.php

--- updateClassAdmin.php
+++ updateClassAdmin.php
@@ -8,13 +8,14 @@ $updateusername = $_POST['TeacherUsername'];
 $sql = "UPDATE class SET ClassName = '$updateclass', TeacherUsername = '$updateusername' WHERE ClassID = '$classID'";
 $exist = "SELECT COUNT(1) AS count FROM teacher WHERE TeacherUsername = '$updateusername'";
 
-$result = mysqli_query($DBconn, $sql);
-$result2 = mysqli_query($DBconn, $exist);
 
-$row = mysqli_fetch_assoc($result2);
+$result1 = mysqli_query($DBconn, $exist);
+
+$row = mysqli_fetch_assoc($result1);
 
 if ($row['count'] == 1){
-      if ($result) {
+   $result2 = mysqli_query($DBconn, $sql);
+      if ($result2) {
          echo "<script>alert('Update Successful!');window.location.href='manageClassAdmin.php'</script>";
       } else {
          echo "Update Failed!: " . mysqli_error($DBconn);
