0aa8e67960490ae2b60bbb75c569bdf6710a77ed
nxthhh <150322618+nxthhh@users.noreply.github.com>
2024-01-14 12:02:29 +0800

    Update removeStudentTeacher.php

--- removeStudentTeacher.php
+++ removeStudentTeacher.php
@@ -2,19 +2,19 @@
 include("connection.php");
 
 //if (isset($_GET['StudentUsername'], $_GET['ClassID'])) {
-  $studentid = $_GET['StudentUsername'];
-//  $classID = $_GET['ClassID'];
+  $studentid = $_GET['id'];
+  $classID = $_GET['classid'];
+  //$id = $_GET['id'];
 
   $sql = "UPDATE student SET ClassID = NULL WHERE StudentUsername = '$studentid'";
   $result = mysqli_query($DBconn, $sql);
   if ($result) {
-     echo "<script>alert('Student Removed!');window.location.href='manageClassTeacher.php'</script>";
-  } else {
+    echo "<script>alert('Student removed successfully!');</script>";
+    echo "<script>window.open('studentListTeacher.php?classID=$classID', '_self')</script>";
+  }else {
      echo "Error removing student!: " . mysqli_error($DBconn);
   }
-//} else {
   echo "Required parameters are missing.";
-//}
 
 mysqli_close($DBconn);
 
